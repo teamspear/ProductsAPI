74c057ed03ccc6ae3d6ef4bfd1a1a1467e00f5ad
Xiao Xie <xiexiaoxiexiao2009@gmail.com>
2019-08-01 19:00:42 -0400

    DB initializtion files

---
+++ schema.sql
@@ -0,0 +1,56 @@
+--drop all tables to have a clean start
+drop table features, photos, related, skus, styles, product;
+-- create an empty products table
+CREATE TABLE IF NOT EXISTS Product
+(
+  id serial NOT NULL PRIMARY KEY,
+  "name" character varying(255),
+  slogan text,
+  "description" text,
+  category character varying(255),
+  default_price integer
+);
+--- as there are bad data, we want to remove some foreignkey constrains, 
+--- we can filter data when querying them;
+CREATE TABLE IF NOT EXISTS Related
+(
+  id serial NOT NULL PRIMARY KEY,
+  product_id integer REFERENCES Product(id),
+  related_product_id integer
+);
+CREATE TABLE IF NOT EXISTS Features
+(
+  id serial NOT NULL PRIMARY KEY,
+  product_id integer REFERENCES Product(id),
+  feature character varying(255),
+  "value" character varying(255)
+);
+
+-- create an empty styles table
+CREATE TABLE IF NOT EXISTS Styles
+(
+  id serial NOT NULL PRIMARY KEY,
+  product_id integer REFERENCES Product(id),
+  name character varying(255),
+  sale_price character varying(50),
+  original_price integer,
+  default_style boolean
+);
+
+-- create an empty photos table
+CREATE TABLE IF NOT EXISTS Photos
+(
+  id serial NOT NULL PRIMARY KEY,
+  styleId integer REFERENCES Styles(id),
+  thumbnail_url character varying(255),
+  "url" character varying(255)
+);
+
+-- create an empty sku table
+CREATE TABLE IF NOT EXISTS SKUs
+(
+  id serial NOT NULL PRIMARY KEY,
+  styleId integer REFERENCES Styles(id),
+  size character varying(50),
+  quantity integer
+);
--- seeding.sql
+++ seeding.sql
@@ -1,15 +1,8 @@
--- create an empty products table
-CREATE TABLE IF NOT EXISTS products
-(
-  id serial NOT NULL,
-  "name" character varying(255),
-  slogan text,
-  "description" text,
-  category character varying(255),
-  default_price integer,
-  CONSTRAINT products_pkey PRIMARY KEY (id)
-)
-
---inject csv to product table
-COPY products
-FROM '/Users/xiao/Desktop/Hack\ Reactor/SDc/Assets/product.csv' DELIMITER ',' CSV HEADER;
+--- no issue
+\copy product FROM 'dataset/product.csv' DELIMITER ',' CSV HEADER;
+--- no issue after removing constriant on related product id
+\copy related FROM 'dataset/related.csv' DELIMITER ',' CSV HEADER;
+\copy features FROM 'dataset/features.csv' DELIMITER ',' CSV;
+\copy styles FROM 'dataset/styles.csv' DELIMITER ',' CSV HEADER;
+\copy skus FROM 'dataset/skus.csv' DELIMITER ',' CSV HEADER;
+\copy photos FROM 'dataset/photos.csv' DELIMITER ',' CSV HEADER FORCE QUOTE "thumbnail_url", "url";
