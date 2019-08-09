b321a9b52483a8a910caa768f3f8091f125ea9ae
Xiao Xie <xiexiaoxiexiao2009@gmail.com>
2019-08-09 16:29:07 -0400

    add load.io

--- server/router.js
+++ server/router.js
@@ -2,6 +2,10 @@ const db = require('./queries');
 const express = require('express');
 const router = express.Router();
 
+//for loader.io load test
+router.get('/loaderio-41d956bc6bf42972bdd147d1b38eed8e', (req, res) => {
+  res.send('loaderio-41d956bc6bf42972bdd147d1b38eed8e');
+});
 router.get('/products/list', db.getProducts);
 router.get('/products/:product_id', db.getProductById);
 router.get('/products/', db.getProductById);
